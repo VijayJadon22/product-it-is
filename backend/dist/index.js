26db74bdb204780c3b666d931b4069dae8c8814c
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-02-18 22:17:05 +0530

    Initialized a empty react project with required dependencies called "product-it-is"

---
+++ backend/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Backend server is starting...");
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+console.log("Backend server is starting.....");
+
+const num1: number = 2;
+const num2: number = 10;
+
+console.log(num1 + num2);
