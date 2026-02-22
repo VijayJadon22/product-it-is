260a673535c0b3737745761cd51a165963841b0b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-02-22 21:08:02 +0530

    Added the express middleware to parse incoming request in JSON format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,13 @@ import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
-const PORT = ENV.PORT || 3002;
+app.use(clerkMiddleware()); // It is a middleware that will check for cookies and set req.auth with the user information if the user is authenticated. It will also set req.session with the session information if the user has an active session. otherwise, it will set req.auth to null and req.session to null.
 
-app.use(clerkMiddleware());
+app.use(express.json()); // It is a middleware that will parse the incoming request body as JSON.
+
+app.use(express.urlencoded({ extended: true })); // It is a middleware that will parse the incoming request body as URL-encoded data and make it available on req.body. The extended option allows for rich objects and arrays to be encoded into the URL-encoded format, which can be useful for complex data structures.
+
+const PORT = ENV.PORT || 3002;
 
 app.get("/", (req, res) => {
   return res.json({ message: "You app is live" });
